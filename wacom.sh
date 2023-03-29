4e99ba060552be164c947b54382b20517be6c6ff
jr636-coala <admin@ab24.xyz>
2023-03-29 22:05:44 +0100

    added wacom.sh

---
+++ wacom.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xsetwacom set "Wacom Intuos PT S Pad pad" Button 3 "key +ctrl +shift = -shift -ctrl"
+xsetwacom set "Wacom Intuos PT S Pad pad" Button 1 "key +ctrl - -ctrl"
+xsetwacom set "Wacom Intuos PT S Pad pad" Button 9 "key +ctrl z -ctrl"
+xsetwacom set "Wacom Intuos PT S Pad pad" Button 8 "key insert"
+xsetwacom set "Wacom Intuos PT S (WL) Pad pad" Button 10 "key +ctrl +shift = -shift -ctrl"
+xsetwacom set "Wacom Intuos PT S (WL) Pad pad" Button 11 "key +ctrl - -ctrl"
+xsetwacom set "Wacom Intuos PT S (WL) Pad pad" Button 9 "key +ctrl z -ctrl"
+xsetwacom set "Wacom Intuos PT S (WL) Pad pad" Button 8 "key insert"
